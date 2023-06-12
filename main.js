721d4662e277c3554c91aec371b5bbc891193675
Awesomegamergame <68134172+awesomegamergame@users.noreply.github.com>
2023-06-11 22:45:33 -0400

    fix the cards on phones and move js to a file

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
-
+document.addEventListener('DOMContentLoaded', function () 
+{
+    const prevBtn = document.querySelector('.prev');
+    const nextBtn = document.querySelector('.next');
+    const gameCards = document.querySelector('.game-cards');
+    const gameCardWidth = document.querySelector('.game-card').offsetWidth;
+    let translateX = 0;
   
+    prevBtn.addEventListener('click', function () 
+    {
+      if (translateX < 0) 
+      {
+        translateX += gameCardWidth + 40; // Adjusted margin value
+        gameCards.style.transform = `translateX(${translateX}px)`;
+      }
+    });
+  
+    nextBtn.addEventListener('click', function () 
+    {
+      const containerWidth = gameCards.offsetWidth;
+      const cardsWidth = gameCards.scrollWidth;
+      if (Math.abs(translateX) < cardsWidth - containerWidth) 
+      {
+        translateX -= gameCardWidth + 40; // Adjusted margin value
+        gameCards.style.transform = `translateX(${translateX}px)`;
+      }
+    });
+});
