7da78897868640ae08c653a7ae26b5cb5d8e6ff9
awesomegamergame <68134172+awesomegamergame@users.noreply.github.com>
2023-08-02 08:36:42 -0400

    added a search function and fixed some bugs

--- rhythm-rampage/levels/main.js
+++ rhythm-rampage/levels/main.js
@@ -57,35 +57,44 @@ function showUserProfile(user) {
 
   profilePicture.src = user.photoURL;
   displayName.textContent = user.displayName;
-  email.textContent = user.email;
 }
   
   // Handle form submission for uploading maps
-  const uploadForm = document.getElementById('upload-form');
-  uploadForm.addEventListener('submit', e => {
-    e.preventDefault();
-  
-    const songName = document.getElementById('song-name').value;
-    const artist = document.getElementById('artist').value;
-    const mapFile = document.getElementById('map-file').files[0];
-  
-    // Upload the map file to Firebase Storage
-    const storageRef = firebase.storage().ref();
-    const mapFileRef = storageRef.child(`maps/${mapFile.name}`);
-    mapFileRef.put(mapFile)
-      .then(snapshot => {
-        // Get the download URL of the uploaded map file
-        return snapshot.ref.getDownloadURL();
-      })
-      .then(downloadURL => {
-        // Save map details (song name, artist, uploader, download link) to Firestore
-        const db = firebase.firestore();
-        db.collection('maps').add({
+const uploadForm = document.getElementById('upload-form');
+uploadForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const songName = document.getElementById('song-name').value;
+  const artist = document.getElementById('artist').value;
+  const mapFile = document.getElementById('map-file').files[0];
+
+  // Preprocess the values for case-insensitive and punctuation-free search
+  const songNameLower = songName.toLowerCase().replace(/[^\w\s]/g, '');
+  const artistLower = artist.toLowerCase().replace(/[^\w\s]/g, '');
+  const uploaderLower = firebase.auth().currentUser.displayName.toLowerCase().replace(/[^\w\s]/g, '');
+
+  // Upload the map file to Firebase Storage
+  const storageRef = firebase.storage().ref();
+  const mapFileRef = storageRef.child(`maps/${mapFile.name}`);
+  mapFileRef
+    .put(mapFile)
+    .then(snapshot => {
+      // Get the download URL of the uploaded map file
+      return snapshot.ref.getDownloadURL();
+    })
+    .then(downloadURL => {
+      // Save map details (song name, artist, uploader, download link) to Firestore
+      const db = firebase.firestore();
+      db.collection('maps')
+        .add({
           songName,
           artist,
           uploader: firebase.auth().currentUser.displayName,
           downloadURL,
-          timestamp: firebase.firestore.FieldValue.serverTimestamp()
+          timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+          songNameLower, // Store case-insensitive and punctuation-free versions
+          artistLower,
+          uploaderLower
         })
         .then(() => {
           // Clear the form after successful upload
@@ -95,11 +104,11 @@ function showUserProfile(user) {
         .catch(error => {
           console.error('Error saving map details:', error);
         });
-      })
-      .catch(error => {
-        console.error('Error uploading map file:', error);
-      });
-  });
+    })
+    .catch(error => {
+      console.error('Error uploading map file:', error);
+    });
+});
 
 // Fetch and display the list of uploaded maps from Firestore
 function displayUploadedMaps() {
@@ -126,3 +135,59 @@ function displayUploadedMaps() {
       console.error('Error fetching uploaded maps:', error);
     });
 }
+
+// Fetch and display the list of uploaded maps from Firestore based on search input
+function searchMaps(searchInput, searchCriteria) {
+  // Preprocess the search input for case-insensitive and punctuation-free search
+  const searchInputLower = searchInput.toLowerCase().replace(/[^\w\s]/g, '');
+
+  const db = firebase.firestore();
+  db.collection('maps')
+    .where(searchCriteria + 'Lower', '>=', searchInputLower)
+    .where(searchCriteria + 'Lower', '<=', searchInputLower + '\uf8ff')
+    .get()
+    .then(snapshot => {
+      const searchResults = document.getElementById('search-results');
+      searchResults.innerHTML = '';
+
+      snapshot.forEach(doc => {
+        const mapData = doc.data();
+        const mapItem = document.createElement('div');
+        mapItem.innerHTML = `
+          <p><strong>Uploader:</strong> ${mapData.uploader}</p>
+          <p><strong>Song Name:</strong> ${mapData.songName}</p>
+          <p><strong>Artist:</strong> ${mapData.artist}</p>
+          <a href="${mapData.downloadURL}" download>Download Map</a>
+          <hr>
+        `;
+        searchResults.appendChild(mapItem);
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching search results:', error);
+    });
+}
+
+const searchForm = document.getElementById('search-form');
+searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const searchInput = document.getElementById('search-input').value.trim();
+  const searchCriteria = document.getElementById('search-criteria').value;
+
+  if (searchInput !== '') {
+    const searchResults = document.getElementById('search-results');
+    searchResults.innerHTML = ''; // Clear previous search results
+
+    searchMaps(searchInput, searchCriteria); // Call the search function with the input
+
+    // Optionally, you can hide the top 5 uploaded maps when showing search results
+    document.getElementById('map-list').style.display = 'none';
+    searchResults.style.display = 'block'; // Display the search results section
+  } else {
+    // If the search input is empty, just display the top 5 uploaded maps
+    document.getElementById('map-list').style.display = 'block';
+    document.getElementById('search-results').style.display = 'none';
+    displayUploadedMaps();
+  }
+});
